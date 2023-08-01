0c2d19b27f8b96760a2bf86c11fcf2d23ec89d3d
rmohan7603 <rmohan7603@gmail.com>
2023-08-01 22:56:05 +0530

    datafilebackend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import products from "../products";
+import { useParams} from "react-router-dom";
+import { useState,useEffect } from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import {
   Row,
@@ -14,8 +15,16 @@ import {
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
-  const { id: productID } = useParams();
-  const product = products.find((p) => p._id === productID);
+  const [product,setProduct]=useState({});
+  const { id: productId } = useParams();
+  useEffect(()=>{
+    const fetchProduct=async()=>{
+    const {data}=await axios.get(`/api/products/${productId}`);
+    setProduct(data);
+    }
+    fetchProduct();
+  },[productId]);
+
 
   return (
     <>
