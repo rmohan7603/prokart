358b3666d52ef1eb8d4b9e95978ed3efaadf7152
rmohan7603 <rmohan7603@gmail.com>
2023-08-20 14:53:11 +0530

    postman complete

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,24 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  req.status(404);
+  next(error);
+};
+
+
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message=err.message;
+
+  //Check for Mongoose bad ObjectID
+  if(err.name==='CastError' && err.kind==='ObjectId'){
+  message=`Resourse not found`;
+  statusCode=404;
+}
+res.status(statusCode).json({
+    message,
+    stack:process.env.NODE_ENV==='production'?'pancake':err.stack,
+})
+};
+
+export {notFound,errorHandler}
