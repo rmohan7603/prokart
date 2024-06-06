30773bc457f5a268a70a52a7e144438f996b5ea9
rmohan7603 <rmohan7603@gmail.com>
2024-06-06 15:17:59 +0530

    PaginateComponentDone

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
-const { data: products, isLoading, error } = useGetProductsQuery();
+
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -15,12 +19,14 @@ const { data: products, isLoading, error } = useGetProductsQuery();
     ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (<>
     <h1>Latest Products</h1>
     <Row>
-      {products.map((product)=>(
+      {data.products.map((product)=>(
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
           </Col>
       ))}
-    </Row></>)}
+    </Row>
+    <Paginate pages={data.pages} page={data.page} />
+    </>)}
 
     </>
   );
