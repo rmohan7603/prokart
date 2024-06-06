fee6ad7c62aa0a6899fd28eef453f2a403d9d089
rmohan7603 <rmohan7603@gmail.com>
2024-06-06 17:52:05 +0530

    PrepToProd

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
@@ -35,8 +31,21 @@ app.get('/api/config/paypal', (req,res) =>res.send({ clientId:
   process.env.PAYPAL_CLIENT_ID
  }));
 
- const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
   
 
 app.use(notFound);
