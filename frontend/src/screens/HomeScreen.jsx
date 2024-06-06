8dd1068f3531c5f88b5764077a436d913565623e
rmohan7603 <rmohan7603@gmail.com>
2024-06-06 15:55:57 +0530

    SearchCompoDone

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
@@ -9,11 +10,12 @@ import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
 
-  const { pageNumber } = useParams();
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { pageNumber, keyword } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
 
   return (
     <>
+    { keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
     { isLoading ? (
       <Loader/>
     ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (<>
@@ -25,7 +27,7 @@ const HomeScreen = () => {
           </Col>
       ))}
     </Row>
-    <Paginate pages={data.pages} page={data.page} />
+    <Paginate pages={data.pages} page={data.page} keyword = {keyword ? keyword : ''}/>
     </>)}
 
     </>
