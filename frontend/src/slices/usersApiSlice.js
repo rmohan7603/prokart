9cc4b05c8bb14955d50e054962a41381d96f1104
rmohan7603 <rmohan7603@gmail.com>
2024-06-06 13:23:10 +0530

    AdminFuncDone

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -30,8 +30,36 @@ export const usersApiSlice=apiSlice.injectEndpoints({
                 body: data,
             }),
         }),
+        getUsers: builder.query({
+            query: () => ({
+              url: USERS_URL,
+            }),
+            providesTags: ['Users'],
+            keepUnusedDataFor: 5,
+        }),
+        deleteUser: builder.mutation({
+            query: (userId) => ({
+              url: `${USERS_URL}/${userId}`,
+              method: 'DELETE',
+            }),
+        }),
+        getUserDetails: builder.query({
+            query: (id) => ({
+              url: `${USERS_URL}/${id}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        updateUser: builder.mutation({
+            query: (data) => ({
+              url: `${USERS_URL}/${data.userId}`,
+              method: 'PUT',
+              body: data,
+            }),
+            invalidatesTags: ['Users'],
+        }),
     }),
 });
 
 export const { useLoginMutation, useLogoutMutation, 
-    useRegisterMutation, useProfileMutation }= usersApiSlice;
+    useRegisterMutation, useProfileMutation, useGetUsersQuery, useDeleteUserMutation,
+useGetUserDetailsQuery, useUpdateUserMutation }= usersApiSlice;
